2e3057c6c5e94a186531e1c3a70bb0021564a017
dwajsman <danielwajsman@mac.com>
2021-11-05 01:14:44 -0300

    fixed! API not sending error messages

--- breedFetcher.js
+++ breedFetcher.js
@@ -5,27 +5,23 @@ const request = require("request");
 // const fetchBreedDescription = (catBreed, callback) => {
 const fetchBreedDescription = function(catBreed, callback) {
   
-  console.log("hi");
-  // let url = 'https://api.thecatapi.com/v1/breeds/searrch?q=' + catBreed;
-  // request(url, function(error, response, body) {
-  //   // if(error) {
-  //   //   console.error('error:', error);
-  //   // };
-  //   // if(error) {
-  //   //   console.log('statusCode:', response && response.statusCode);
-  //   // } // Print the response status code if a response was received
-  //   const data = JSON.parse(body);
+  //console.log("hi");
+  let url = 'https://api.thecatapi.com/v1/breeds/search?q=' + catBreed;
+  request(url, function(error, response, body) {
+    if(error) {
+      callback(null, data[0].description);
+    }; 
+
+    const data = JSON.parse(body);
     
-  //   console.log(data);
+    //console.log(data);
   
-  //   callback(null, data[0].description);
+    callback(null, data[0].description);
 
-  // });
-
-  // callback(null, "hello");
+  });
 
 };
 
 
 
-module.exports = { fetchBreedDescription };
+module.exports = {fetchBreedDescription};
--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const fetchBreedDescription = require('./breedFetcher');
+const {fetchBreedDescription} = require('./breedFetcher');
 
 let catBreed = process.argv[2];
 
 fetchBreedDescription(catBreed, (error, description) => {
   
-    // if (description.length === 0) {
-    //   console.log("Something went wrong");
-    // }
-    // if (description.length > 0) {
-      console.log("\n", description, error, "\n");
-    // }
+    if (description.length === 0) {
+      console.log("Something went wrong", error);
+    }
+    if (description.length > 0) {
+      console.log("\n", description, "\n");
+    }
 
 });
 
