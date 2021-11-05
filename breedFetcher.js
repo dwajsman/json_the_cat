719eac4b257f81c01f0a638eb8e0325ef8010818
dwajsman <danielwajsman@mac.com>
2021-11-05 01:22:33 -0300

    after linting

--- breedFetcher.js
+++ breedFetcher.js
@@ -4,18 +4,18 @@ const request = require("request");
 
 // const fetchBreedDescription = (catBreed, callback) => {
 const fetchBreedDescription = function(catBreed, callback) {
-  
+
   //console.log("hi");
   let url = 'https://api.thecatapi.com/v1/breeds/search?q=' + catBreed;
-  request(url, function(error, response, body) {
-    if(error) {
+  request(url, function (error, response, body) {
+    if (error) {
       callback(null, data[0].description);
-    }; 
+    }
 
     const data = JSON.parse(body);
-    
+
     //console.log(data);
-  
+
     callback(null, data[0].description);
 
   });
@@ -24,4 +24,6 @@ const fetchBreedDescription = function(catBreed, callback) {
 
 
 
-module.exports = {fetchBreedDescription};
+module.exports = {
+  fetchBreedDescription
+};
--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const {fetchBreedDescription} = require('./breedFetcher');
+const {
+  fetchBreedDescription
+} = require('./breedFetcher');
 
 let catBreed = process.argv[2];
 
 fetchBreedDescription(catBreed, (error, description) => {
-  
-    if (description.length === 0) {
-      console.log("Something went wrong", error);
-    }
-    if (description.length > 0) {
-      console.log("\n", description, "\n");
-    }
+
+  if (description.length === 0) {
+    console.log("Something went wrong", error);
+  }
+  if (description.length > 0) {
+    console.log("\n", description, "\n");
+  }
 
 });
-
-
